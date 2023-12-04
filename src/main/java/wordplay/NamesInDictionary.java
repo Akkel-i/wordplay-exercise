48d3f69000b498e8220a194b3b2ebf65d4737304
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2023-12-04 08:57:12 +0200

    Update NamesInDictionary.java

--- src/main/java/wordplay/NamesInDictionary.java
+++ src/main/java/wordplay/NamesInDictionary.java
@@ -1,9 +1,11 @@
 package wordplay;
 
-import java.util.List;
-
 import wordplay.io.DictionaryReader;
 import wordplay.io.NamesReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class NamesInDictionary {
 
@@ -25,5 +27,58 @@ public class NamesInDictionary {
          *
          * Good luck!
          */
+        List<String> miehetNimetStringina = new ArrayList<>();
+        List<String> naisetNimetStringina = new ArrayList<>();
+        List<String> sanakirjaSanat = new ArrayList<>();
+        Scanner miehetLukija = null;
+        Scanner naisetLukija = null;
+        Scanner sanakirjaLukija = null;
+
+        try {
+            miehetLukija = new Scanner(new File("data/etunimitilasto-miehet-ensimmainen.csv"));
+            System.out.println("Luetaan tiedostoa");
+            while (miehetLukija.hasNext()) {
+                // System.out.println(miehetLukija.nextLine());
+                miehetNimetStringina.add(miehetLukija.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            miehetLukija.close();
+        }
+
+        try {
+            naisetLukija = new Scanner(new File("data/etunimitilasto-naiset-ensimmainen.csv"));
+            System.out.println("Luetaan tiedostoa");
+            while (naisetLukija.hasNext()) {
+                // System.out.println(miehetLukija.nextLine());
+                naisetNimetStringina.add(naisetLukija.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            naisetLukija.close();
+        }
+
+        try {
+            sanakirjaLukija = new Scanner(new File("data/kaikkisanat.txt"));
+            System.out.println("Luetaan tiedostoa");
+            while (sanakirjaLukija.hasNext()) {
+                // System.out.println(miehetLukija.nextLine());
+                sanakirjaSanat.add(sanakirjaLukija.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            sanakirjaLukija.close();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(miehetNimetStringina[i]);
+        }
+
     }
 }
